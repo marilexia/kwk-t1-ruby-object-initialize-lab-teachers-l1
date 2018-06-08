979da19276bbbed029313f169d6f3fa42574894f
Marilexia Faith Acelajado <faithacelajado@gmail.com>
2018-06-08 13:46:05 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,12 +2,14 @@ class Dog
 
   def initialize(name, breed)
     @name = name
-    @breed = breed 
+    @breed = breed
   end
 
   def name=(name, breed)
     @name = name
     @breed = breed
+      if @breed != breed
+        @breed = "Mutt"
   end
 
   def name
